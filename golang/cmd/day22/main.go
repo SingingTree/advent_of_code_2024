1a428eea5bf497bbb0af4c8e3e053a4af1392b27
Bryce Seager van Dyk <bryce@vandyk.net.nz>
2024-12-30 08:44:35 +1300

    22: go part 2

--- golang/cmd/day22/main.go
+++ golang/cmd/day22/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bufio"
 	_ "embed"
+	"fmt"
+	"gonum.org/v1/gonum/stat/combin"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -39,6 +42,10 @@ func (s secretNum) iterate() secretNum {
 	return newNum
 }
 
+func (s secretNum) price() int {
+	return int(s) % 10
+}
+
 type secretNumFinder struct {
 	nextLookup map[secretNum]secretNum
 }
@@ -56,6 +63,32 @@ func (snf *secretNumFinder) findNext(sn secretNum) secretNum {
 		snf.nextLookup[sn] = next
 	}
 	return next
+}
+
+type priceChangeQuad struct {
+	first  int
+	second int
+	third  int
+	fourth int
+}
+
+var possiblePriceChanges = []int{
+	-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+}
+
+func generateAllPossibleSequences() []priceChangeQuad {
+	indices := combin.Cartesian([]int{19, 19, 19, 19})
+	sequences := make([]priceChangeQuad, len(indices))
+	for i := range indices {
+		sequences[i] = priceChangeQuad{
+			first:  possiblePriceChanges[indices[i][0]],
+			second: possiblePriceChanges[indices[i][1]],
+			third:  possiblePriceChanges[indices[i][2]],
+			fourth: possiblePriceChanges[indices[i][3]],
+		}
+	}
+
+	return sequences
 }
 
 func main() {
@@ -80,4 +113,53 @@ func main() {
 		sum += int(sn)
 	}
 	println(sum)
+
+	// Part 2
+	prices := make([][]int, len(secretNums))
+	priceChanges := make([][]int, len(secretNums))
+	for i, sn := range secretNums {
+		prices[i] = make([]int, 2001)
+		prices[i][0] = sn.price()
+		priceChanges[i] = make([]int, 2001)
+		priceChanges[i][0] = -100
+
+		for j := range 2000 {
+			sn = snf.findNext(sn)
+			prices[i][j+1] = sn.price()
+			priceChanges[i][j+1] = prices[i][j+1] - prices[i][j]
+		}
+	}
+
+	containsQuadMap := make([]map[priceChangeQuad]int, len(priceChanges))
+	for i := range priceChanges {
+		containsQuadMap[i] = make(map[priceChangeQuad]int)
+		// Start this loop at 4 instead of 3, as price change 0 is invalid, so we skip it.
+		for j := 4; j < len(priceChanges[i]); j += 1 {
+			pcq := priceChangeQuad{
+				first:  priceChanges[i][j-3],
+				second: priceChanges[i][j-2],
+				third:  priceChanges[i][j-1],
+				fourth: priceChanges[i][j],
+			}
+			if _, ok := containsQuadMap[i][pcq]; ok {
+				// We already have this quad.
+				continue
+			}
+			containsQuadMap[i][pcq] = j
+		}
+	}
+
+	sequences := generateAllPossibleSequences()
+	payoutPerSequence := make([]int, len(sequences))
+	for i := range sequences {
+		for j := range prices {
+			if idx, ok := containsQuadMap[j][sequences[i]]; ok {
+				payoutPerSequence[i] += prices[j][idx]
+			}
+		}
+	}
+	maxPayout := slices.Max(payoutPerSequence)
+	index := slices.Index(payoutPerSequence, maxPayout)
+	fmt.Println(sequences[index])
+	fmt.Println(maxPayout)
 }
